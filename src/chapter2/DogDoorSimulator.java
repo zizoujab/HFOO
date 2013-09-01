475d8df8fe8c1e265c40f1e72dcdde3cf02fe7c2
zizoujab <zizoujab@zizoujab-Aspire-5670.(none)>
2013-09-01 20:49:15 +0100

    Source code of second chapter

---
+++ src/chapter2/DogDoorSimulator.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+public class DogDoorSimulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		DogDoor door = new DogDoor();
+		Remote remote = new Remote(door);
+		System.out.println("Fido barks to go outside ... ");
+		remote.pressButton();
+		System.out.println("\nFido has gone outside ....");
+		remote.pressButton();
+		System.out.println("\nFido's all done ...");
+		remote.pressButton();
+		System.out.println("\nFido's back inside");
+		remote.pressButton();
+
+	}
+
+}
